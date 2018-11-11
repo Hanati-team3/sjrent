adb436a1f32e96ea0a82ccb4b56de7b0c1f63653
gloomycloud <battlecloud@naver.com>
2018-11-11 12:00:38 +0900

    리뷰 컨트롤러까지 함

--- java/kr/or/kosta/sjrent/review/controller/ReviewUpdateController.java
+++ java/kr/or/kosta/sjrent/review/controller/ReviewUpdateController.java
@@ -1,71 +1,63 @@
 package kr.or.kosta.sjrent.review.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import java.util.List;
-import java.util.Map;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 import kr.or.kosta.sjrent.common.controller.Controller;
 import kr.or.kosta.sjrent.common.controller.ModelAndView;
 import kr.or.kosta.sjrent.common.factory.XMLObjectFactory;
-import kr.or.kosta.sjrent.model.service.ModelService;
-import kr.or.kosta.sjrent.model.service.ModelServiceImpl;
-import kr.or.kosta.sjrent.rent.domain.Rent;
-import kr.or.kosta.sjrent.rent.service.RentService;
-import kr.or.kosta.sjrent.rent.service.RentServiceImpl;
-import kr.or.kosta.sjrent.user.domain.User;
-import kr.or.kosta.sjrent.user.service.UserService;
-import kr.or.kosta.sjrent.user.service.UserServiceImpl;
+import kr.or.kosta.sjrent.review.domain.Review;
+import kr.or.kosta.sjrent.review.service.ReviewService;
+import kr.or.kosta.sjrent.review.service.ReviewServiceImpl;
 
 /**
- * rent id를 받아서 db에서 렌트 취소 처리 
+ * Review add 처리
+ * 
  * @author 남수현
  */
 public class ReviewUpdateController implements Controller {
 	private XMLObjectFactory factory;
-	private UserService userService;
-	private RentService rentService;
-	private ModelService modelService;
+	private ReviewService reviewService;
 	private ModelAndView mav;
 
-   @Override
-   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-         throws ServletException {
-      factory = (XMLObjectFactory) request.getServletContext().getAttribute("objectFactory");
-      rentService = (RentService) factory.getBean(RentServiceImpl.class);
-      
-      String inputRentSeq = request.getParameter("rentSeq");
-      if(inputRentSeq != null && !inputRentSeq.equals("")) {
-          int rentSeq = Integer.parseInt(inputRentSeq);
-          boolean result = false;
-    	  PrintWriter out = null;
-		  try {
-			out = response.getWriter();
-		  } catch (IOException e2) {
-		  }
-    	  try {
-			result = rentService.cancel(rentSeq);
-		  } catch (Exception e1) {
-			out.println("cancelError : "+e1);
-			return null;
-		  }
-    	  if(result) {
-    		  out.println("success");
-    		  return null;
-    	  }else {
-    		  out.println("fail");
-    		  return null;
-    	  }
-      }
-	return null;
-   }
-   
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException {
+		factory = (XMLObjectFactory) request.getServletContext().getAttribute("objectFactory");
+		reviewService = (ReviewService) factory.getBean(ReviewServiceImpl.class);
+		String reviewNumberS = request.getParameter("number");
+		String modelName = request.getParameter("modelName");
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		String picture = request.getParameter("picture");
+		String evalScoreS = request.getParameter("evalScore");
+		int reviewNumber = 0;
+		int evalScore = 0;
+		if (evalScoreS != null && !evalScoreS.equals("")) {
+			evalScore = Integer.parseInt(evalScoreS);
+		}
+		if(reviewNumberS != null && !reviewNumberS.equals("")) {
+			reviewNumber = Integer.parseInt(reviewNumberS);
+		}
+		Review review = new Review();
+		review.setNumber(reviewNumber);
+		review.setTitle(title);
+		review.setPicture(picture);
+		review.setModelName(modelName);
+		review.setEvalScore(evalScore);
+		review.setContent(content);
+		try {
+			if (reviewService.update(review)) {
+				mav.addObject("message", "update_success");
+			} else {
+				mav.addObject("message", "update_fail");
+			}
+		} catch (Exception e) {
+			mav.addObject("message", "update_error:"+e);
+		}
+		mav.setView("/mypage/myReviewList.jsp");
+		return mav;
+	}
+
 }
