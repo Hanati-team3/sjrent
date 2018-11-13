5fb90ffee0cc799b12293f068237743c4b56d882
gloomycloud <battlecloud@naver.com>
2018-11-13 19:26:35 +0900

    rentcontroller 수정

--- java/kr/or/kosta/sjrent/review/controller/ReviewDeleteController.java
+++ java/kr/or/kosta/sjrent/review/controller/ReviewDeleteController.java
@@ -1,5 +1,6 @@
 package kr.or.kosta.sjrent.review.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import kr.or.kosta.sjrent.common.controller.Controller;
 import kr.or.kosta.sjrent.common.controller.ModelAndView;
 import kr.or.kosta.sjrent.common.factory.XMLObjectFactory;
+import kr.or.kosta.sjrent.common.listener.ServletContextLoadListener;
 import kr.or.kosta.sjrent.review.domain.Review;
 import kr.or.kosta.sjrent.review.service.ReviewService;
 
@@ -34,10 +36,17 @@ public class ReviewDeleteController implements Controller {
 			throws ServletException {
 		factory = (XMLObjectFactory) request.getServletContext().getAttribute("objectFactory");
 		reviewService = (ReviewService) factory.getBean(ReviewService.class);
-
+		Review review = null;
 		String inputReviewSeq = request.getParameter("reviewSeq");
 		if (inputReviewSeq != null && !inputReviewSeq.equals("")) {
 			int reviewSeq = Integer.parseInt(inputReviewSeq);
+			try {
+				review = reviewService.read(reviewSeq);
+				review.getPicture();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			boolean result = false;
 			PrintWriter out = null;
 			try {
@@ -47,6 +56,14 @@ public class ReviewDeleteController implements Controller {
 			try {
 				if (reviewService.delete(reviewSeq)) {
 					mav.addObject("message", "delete_success");
+					if(review != null) {
+						String uploadPath = ServletContextLoadListener.class.getResource("").getPath();
+						uploadPath = uploadPath.substring(0,uploadPath.indexOf("/WEB-INF"))+"/images/review";
+						File file = new File(uploadPath+"/"+review.getPicture());
+						if(file.exists()) {
+							file.delete();
+						}
+					}
 				} else {
 					mav.addObject("message", "delete_fail");
 				}
